8163dc8915098c61c257be1aa5d6ce856c28b788
PolaX3 <99782012+PolaX3@users.noreply.github.com>
2024-12-15 02:13:54 +0700

    Removed service, test, and clear button to prevent conflict with Prime and older mixes

--- hook.cpp
+++ hook.cpp
@@ -25,7 +25,6 @@ const uint8_t PAD_RD = 0x10;
 const uint8_t CAB_SERVICE = 0x40;
 const uint8_t CAB_TEST = 0x02;
 const uint8_t CAB_COIN = 0x04;
-const uint8_t CAB_CLEAR = 0x80;
 
 uint8_t IOSTATE[4] = { 0xFF, 0xFF, 0xFF, 0xFF };
 
@@ -50,9 +49,6 @@ const std::list<piu_bind> binds = {
     { "KP_3",  STATE_PLAYER_2, PAD_RD },
     { "F6",    STATE_CAB_PLAYER_2, CAB_COIN },
 
-    { "F1", STATE_CAB_PLAYER_1, CAB_TEST },
-    { "F2", STATE_CAB_PLAYER_1, CAB_SERVICE },
-    { "F3", STATE_CAB_PLAYER_1, CAB_CLEAR },
 };
 
 bool is_real_pad_connected = false;
